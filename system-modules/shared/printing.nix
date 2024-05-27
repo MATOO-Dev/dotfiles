0e24bc864d2d5fb3ad8693e45340e59e399af5df
MATOO-Dev <matoo-dev@proton.me>
2024-05-27 21:40:26 +0200

    Updated flake, added betterdiscord, fixed printing

--- system-modules/shared/printing.nix
+++ system-modules/shared/printing.nix
@@ -1,9 +1,16 @@
-{ lib, config, pkgs, ... }:
 {
-# Enable CUPS to print documents.
-	services.printing.enable = true;
-	# Enable printer driver
-	environment.systemPackages = with pkgs; [
-		hplip
-	];
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+
+    drivers = with pkgs; [ hplip ];
+  };
+
+  environment.systemPackages = with pkgs; [ hplip ];
 }
