65cdeb1a7513b0bfa58c818fc224424e97bfe596
MATOO-Dev <matoo-dev@proton.me>
2024-06-04 11:07:17 +0200

    Working dap setup

--- home-modules/nixvim/dap.nix
+++ home-modules/nixvim/dap.nix
@@ -3,13 +3,12 @@
   programs.nixvim.plugins.dap = {
     enable = true;
     extensions = {
-      dap-ui.enable = true;
       dap-virtual-text.enable = true;
     };
     adapters = {
       executables = {
         lldb = {
-          command = "lldb";
+          command = "lldb-dap";
         };
         gdb = {
           command = "gdb";
@@ -24,13 +23,49 @@
     configurations = {
       cpp = [
         {
-          type = "gdb";
-          request = "launch";
           name = "Launch C++ debugger";
+          request = "launch";
+          type = "gdb";
+          program.__raw = ''
+            function()
+            local path = vim.fn.input({
+              prompt = 'Path to executable: ',
+              default = vim.fn.getcwd() .. '/',
+              completion = 'file',
+            })
+
+            return (path and path ~= "") and path or require("dap").ABORT
+            end
+          '';
         }
       ];
     };
   };
+
+  programs.nixvim.plugins.dap.extensions.dap-ui = {
+    enable = true;
+  };
+
+  programs.nixvim.keymaps = [
+    {
+      action = ":lua require('dap').toggle_breakpoint()<CR>";
+      key = "<leader>bb";
+      mode = "n";
+      options.silent = true;
+    }
+    {
+      action = ":lua require('dap').continue()<CR>";
+      key = "<F5>";
+      mode = "n";
+      options.silent = true;
+    }
+    {
+      action = ":lua require('dapui').toggle()<CR>";
+      key = "<leader>dap";
+      mode = "n";
+      options.silent = true;
+    }
+  ];
 
   home.packages = with pkgs; [
     lldb
