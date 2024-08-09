50d3504a45a629fe022477eb2c20be170452b5c2
MATOO <fagoldob@gmail.com>
2024-08-09 10:03:20 +0200

    Changed nvidia driver to stable

--- system-modules/desktop/nvidia.nix
+++ system-modules/desktop/nvidia.nix
@@ -8,8 +8,8 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
     modesetting.enable = true;
     open = false;
   };
