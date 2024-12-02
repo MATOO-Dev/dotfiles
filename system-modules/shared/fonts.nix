10b63dc2d81832d2a11c8b4b4827a74e177bff8d
MATOO-Dev <matoo-dev@proton.me>
2024-12-02 16:55:00 +0100

    Split nerd-fonts

--- system-modules/shared/fonts.nix
+++ system-modules/shared/fonts.nix
@@ -5,8 +5,78 @@
   ...
 }:
 {
-  fonts.packages = with pkgs; [
-    nerdfonts
-    paratype-pt-sans
-  ];
+	fonts.packages = ( with pkgs; [
+		paratype-pt-sans
+	]) ++ (with pkgs.nerd-fonts; [
+		# this is a really ugly hack
+		# todo: clean this up
+		_0xproto
+		_3270
+		agave
+		anonymice
+		arimo
+		aurulent-sans-mono
+		bigblue-terminal
+		bitstream-vera-sans-mono
+		blex-mono
+		caskaydia-cove
+		caskaydia-mono
+		code-new-roman
+		comic-shanns-mono
+		commit-mono
+		cousine
+		d2coding
+		daddy-time-mono
+		departure-mono
+		dejavu-sans-mono
+		droid-sans-mono
+		envy-code-r
+		fantasque-sans-mono
+		fira-code
+		fira-mono
+		geist-mono
+		go-mono
+		gohufont
+		hack
+		hasklug
+		heavy-data
+		hurmit
+		im-writing
+		inconsolata
+		inconsolata-go
+		inconsolata-lgc
+		intone-mono
+		iosevka
+		iosevka-term
+		iosevka-term-slab
+		jetbrains-mono
+		lekton
+		liberation
+		lilex
+		martian-mono
+		meslo-lg
+		monaspace
+		monofur
+		monoid
+		mononoki
+		mplus
+		noto
+		open-dyslexic
+		overpass
+		profont
+		proggy-clean-tt
+		recursive-mono
+		roboto-mono
+		shure-tech-mono
+		sauce-code-pro
+		space-mono
+		symbols-only
+		terminess-ttf
+		tinos
+		ubuntu
+		ubuntu-mono
+		ubuntu-sans
+		victor-mono
+		zed-mono
+	]);
 }
