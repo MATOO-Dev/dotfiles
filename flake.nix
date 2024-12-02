6c8478f194c693e942f1843e99651bfa86d68ac0
MATOO-Dev <matoo-dev@proton.me>
2024-12-02 11:55:35 +0100

    Started setup for new config

--- flake.nix
+++ flake.nix
@@ -98,6 +98,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nvim-config = {
+      url = "github:matoo-dev/nvim-config";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
