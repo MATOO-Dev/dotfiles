fc7ecc11b5dd22ea23af35fdf61d373e99e3602c
MATOO-Dev <MATOO-Dev@proton.me>
2025-03-31 12:24:29 +0200

    Adjusted hyprland, decman and nvim configs

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -4,11 +4,12 @@ return {
 		opts = {
 			format_on_save = {
 				timeout_ms = 500,
-				lsp_format = 'fallback',
+				-- lsp_format = 'fallback',
 			},
 			formatters_by_ft = {
 				cpp = { 'clang-format' },
 				rust = { 'rustfmt' },
+				conform = {},
 			},
 		}
 	},
