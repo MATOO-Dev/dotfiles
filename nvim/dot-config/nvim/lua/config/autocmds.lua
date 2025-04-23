8474c2c25a3e0954f72697e2df6668530ad50189
MATOO <franz.gold@stud.hs-kempten.de>
2025-04-23 22:00:54 +0200

    Config sync

--- nvim/dot-config/nvim/lua/config/autocmds.lua
+++ nvim/dot-config/nvim/lua/config/autocmds.lua
@@ -14,14 +14,18 @@ vim.cmd('autocmd BufReadPost *.md setlocal wrap')
 vim.cmd('autocmd BufReadPost *.typ TypstPreview')
 
 -- automatically connect to godothost server
+GDConnected = false
 vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
 	desc = 'Connect to godothost',
 	pattern = { '*.gd', '*.gdshader', '*.tscn', '*.tres' },
 	callback = function()
+		if GDConnected then
+			return end
 		local project_file = vim.fn.getcwd() .. '/project.godot'
 		if project_file then
 			vim.fn.serverstart './godothost'
 			print("Connected to godothost")
+			GDConnected = true
 		end
 	end,
 })
