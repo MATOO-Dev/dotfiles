bed1e6d3664b09543a91670d7bfe58189f06f415
MATOO <franz.gold@stud.hs-kempten.de>
2025-05-28 12:34:38 +0200

    Updated hyprland & nvim confs

--- nvim/dot-config/nvim/lua/plugins/mason.lua
+++ nvim/dot-config/nvim/lua/plugins/mason.lua
@@ -1,11 +1,13 @@
 return {
 	{
 		'williamboman/mason.nvim',
+		version = "^1.0.0",
 		event = 'VeryLazy',
 		opts = {}
 	},
 	{
 		'williamboman/mason-lspconfig.nvim',
+		version = "^1.0.0",
 		opts = {
 			automatic_installation = true,
 		},
